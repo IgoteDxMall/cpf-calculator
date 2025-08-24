0c0b12fbcf0f2006e0b9f1d9b09fdf45edfa7e26
IgoteDxMall <igorsantosgomes312@gmail.com>
2025-08-23 21:14:35 -0300

    Primeiro commit - Projeto cálculo de CPF

---
+++ App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite os 9 primeiros dígitos do CPF (sem pontos ou traços): ");
+        String cpfInput = scanner.nextLine();
+
+        while (cpfInput.length() != 9) {
+            System.out.print("Entrada inválida. Por favor, digite exatamente 9 dígitos numéricos: ");
+            cpfInput = scanner.nextLine();
+        }
+
+        CalculoCPF calculoCPF = new CalculoCPF(cpfInput);
+        calculoCPF.calcularPrimeiroDigito();
+        calculoCPF.calcularSegundoDigito();
+
+        System.out.println("CPF completo: " + calculoCPF.getCPFCompleto());
+
+    }
+}
---
+++ CalculoCPF.java
@@ -0,0 +1,46 @@
+public class CalculoCPF {
+    private String cpf;
+    private int[] digitoCPF;
+    private int primeiroDigito;
+    private int segundoDigito;
+
+    public CalculoCPF(String cpf) {
+        this.cpf = cpf;
+        this.digitoCPF = new int[11];
+        this.primeiroDigito = 0;
+        this.segundoDigito = 0;
+    }
+
+    public void calcularPrimeiroDigito() {
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
+            digitoCPF[i] = Integer.parseInt(String.valueOf(cpf.charAt(i)));
+            soma += digitoCPF[i] * (10 - i);
+        }
+        int resto = soma % 11;
+        primeiroDigito = (resto < 2) ? 0 : 11 - resto;
+    }
+
+    public void calcularSegundoDigito() {
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
+            soma += digitoCPF[i] * (11 - i);
+        }
+        soma += primeiroDigito * 2; // inclui o primeiro dígito no cálculo
+
+        int resto = soma % 11;
+        segundoDigito = (resto < 2) ? 0 : 11 - resto;
+    }
+
+    public int getPrimeiroDigito() {
+        return primeiroDigito;
+    }
+
+    public int getSegundoDigito() {
+        return segundoDigito;
+    }
+
+    public String getCPFCompleto() {
+        return cpf + primeiroDigito + segundoDigito;
+    }
+}
